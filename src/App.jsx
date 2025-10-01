81e1144241489949b83d37391b3a95907e8663b2
bear080604 <phuonganhkhanh683@gmail.com>
2025-10-01 17:18:13 +0700

    about

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@
 import './App.css';
 import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
+import About from './pages/About/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Layout>
-        
-          <Home />
-      </Layout>
+     <Layout>
+  <Router>  
+    <Routes>  
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+</Layout>
     </>
   );
 }
